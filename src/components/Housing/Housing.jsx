d27754ccb5c6c31fc51b4640cd948f7bb5228f1d
AndrewZajac <andrewzajac05@gmail.com>
2024-06-26 16:24:23 -0500

    Housing page to assign residents is styled up, brought in ListGroup to organize them and make them easier to read

--- src/components/Housing/Housing.jsx
+++ src/components/Housing/Housing.jsx
@@ -1,9 +1,23 @@
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import Button from "react-bootstrap/Button";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import './Housing.css';
+import {
+  Container,
+  Navbar,
+  Nav,
+  Button,
+  Dropdown,
+  InputGroup,
+  Card,
+  Row,
+  Col,
+  Alert,
+  Modal,
+  ListGroup,
+} from "react-bootstrap";
 
 function AssignResident() {
   const housing = useSelector((store) => store.housingReducer);
@@ -28,24 +42,40 @@ function AssignResident() {
   return (
     <div className="container">
       <h2>Housing</h2>
-      <div>
-        <Button onClick={() => history.push("/residents")}>
-          Back to Active Residents
-        </Button>
-      </div>
-      {housing
-        .filter((h) => !h.resident_id)
-        .map((house, i) => (
-          <div key={i}>
-            <div>
-              room number: {house.room_number}, hall: {house.hall}, floor:{" "}
-              {house.floor}
-              <Button onClick={() => handleAssignRoom(house.room_number)}>
-                Assign Room
-              </Button>
-            </div>
-          </div>
-        ))}
+      <Row>
+        <div>
+          <button onClick={() => history.push("/residents")}>
+            Back to Active Residents
+          </button>
+        </div>
+        <Col xs={12} md={6}>
+          <Card style={{border: "0"}}>
+            <Card.Body>
+              <Card.Title>Available Rooms</Card.Title>
+              <ListGroup >
+                {housing
+                  .filter((h) => !h.resident_id)
+                  .map((house, i) => (
+                    <div key={i}>
+                      <ListGroup.Item>
+                        <div>
+                          <strong>Room Number:</strong> {house.room_number},
+                          <strong> Hall:</strong> {house.hall},
+                          <strong> Floor:</strong> {house.floor} 
+                          <button className="assignroombtn"
+                            onClick={() => handleAssignRoom(house.room_number)}
+                          >
+                            Assign Room
+                          </button>
+                        </div>
+                      </ListGroup.Item>
+                    </div>
+                  ))}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
