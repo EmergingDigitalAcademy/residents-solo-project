7ea4c01da5f55e073c4f31c7d4906daf744a6422
AndrewZajac <andrewzajac05@gmail.com>
2024-06-27 13:31:30 -0500

    landing page has a bg img and also everything on the page is now placed inside of a card with a lighter gray bg so it can be read easier

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,43 +1,57 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import './LandingPage.css';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./LandingPage.css";
+import { Card } from "react-bootstrap";
 
 // CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
+import RegisterForm from "../RegisterForm/RegisterForm";
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const [heading, setHeading] = useState("Welcome");
   const history = useHistory();
 
   const onLogin = (event) => {
-    history.push('/login');
+    history.push("/login");
   };
 
   return (
     <div className="landingPage vh-100 vh-100">
       <div className="container">
-      <h2>{heading}</h2>
-
-      <div className="grid">
-        <div className="grid-col grid-col_8">
-          <p>
-            This application is made for CNA's to chart tasks on how much assistance the resident needed.
-          </p>
-          <p>
-            Also, an admin will be able to add and assign residents to their room.
-          </p>
+        <Card
+          style={{
+            backgroundColor: "#efefef",
+            width: "150px",
+            alignItems: "center",
+            marginLeft: "8px"
+          }}
+        >
+          <h2>{heading}</h2>
+        </Card>
+        <div className="grid">
+          <div className="grid-col grid-col_8">
+            <Card style={{backgroundColor: "#efefef", width: "500px", alignItems: "center"}}>
+              <p>
+                This application is made for CNA's to chart tasks on how much
+                assistance each resident needs.
+              </p>
+              <p>
+                Also, an admin will be able to add and assign residents to their
+                room.
+              </p>
+            </Card>
+          </div>
+          <div className="grid-col grid-col_4">
+            <RegisterForm />
+            <Card style={{ backgroundColor: "#efefef", width: "250px", marginLeft: "8px" }}>
+              <center className="registerLandingPage">
+                <h4>Already a User?</h4>
+                <button className="btn btn_sizeSm, login" onClick={onLogin}>
+                  Login
+                </button>
+              </center>
+            </Card>
+          </div>
         </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
-          <center className="registerLandingPage">
-            <h4>Already a User?</h4>
-            <button className="btn btn_sizeSm, login" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
-      </div>
       </div>
     </div>
   );
